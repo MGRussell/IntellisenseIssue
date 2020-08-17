7df38b9873a3bbc7c95b75028fc2bf74f9d5488c
MGRussell <yeahrussell@gmail.com>
2020-08-17 19:15:11 +0700

    Add project files.

---
+++ IntellisenseIssue/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntellisenseIssue
+{
+    class ViewModel
+    {
+        public string SomeString { get; set; } = "String from ViewModel";
+    }
+}
